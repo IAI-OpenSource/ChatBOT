38fceaf07b7ed818e5f6fc9e3e3b5f924dacb8e3
sabour-agoro <abdoulsabouragoro@gmail.com>
2026-02-10 16:24:18 +0100

    cors ajout des localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,13 @@ app = FastAPI(title="Chatbot API")
 # Setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    origins = [
+        
+        "http://localhost",
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:3000",
+ ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
